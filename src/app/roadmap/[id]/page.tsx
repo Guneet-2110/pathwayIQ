2adb089d1104e754fd3c608b0b71d53b00f4c89d
Guneet-2110 <guneet.ar2010@gmail.com>
2026-05-27 00:39:16 -0500

    Update page.tsx

--- src/app/roadmap/[id]/page.tsx
+++ src/app/roadmap/[id]/page.tsx
@@ -1,5 +1,7 @@
 'use client'
 
+import ChecklistItem from '@/components/ChecklistItem'
+import ProgressBar from '@/components/ProgressBar'
 import { useEffect, useState } from 'react'
 import { useParams, useRouter } from 'next/navigation'
 import { createClient } from '@/lib/supabase'
